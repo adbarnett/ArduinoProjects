80d359e318e54f7c067e7be170cffb66a163ce56
Andrew <adbarnett@gmail.com>
2015-02-19 19:42:25 +0000

    reaction game: penalty for early press and added reaction time

--- ReactionTestGame/ReactionTestGame.ino
+++ ReactionTestGame/ReactionTestGame.ino
@@ -15,40 +15,80 @@ void setup() {
   pinMode(button1, INPUT);     
   pinMode(button2, INPUT);
   Serial.begin(9600);
-  startTime = millis();
+  
 }
 
 void loop(){
   if (!gameOver)
-  {  
-    if (digitalRead(button1) == 0 && gameStarted) {    
-      reactionTime = millis() - startTime;
-      gameOver = true;
-      digitalWrite(led1, HIGH);
-      Serial.print("Player 1 wins - ");
-      Serial.print(reactionTime);
-      Serial.println();
-      delay(2000);
-      digitalWrite(led1, LOW);
+  {     
+    if (!gameStarted)
+    {
+      while (millis() < 5000 && !gameOver)
+      {
+        if (digitalRead(button1) == 0) {
+          digitalWrite(led2, HIGH);
+          Serial.print("Player 2 wins. Player 1 pressed too soon");
+          Serial.println();
+          delay(2000);
+          digitalWrite(led2, LOW);
+          gameOver = true;
+          Serial.println("Game Over");
+          Serial.println("Press reset");
+        }
+      
+        if (digitalRead(button2) == 0) {
+          digitalWrite(led1, HIGH);
+          Serial.print("Player 1 wins. Player 2 pressed too soon");
+          Serial.println();
+          gameOver = true;
+          delay(2000);
+          digitalWrite(led1, LOW);
+          Serial.println("Game Over");
+          Serial.println("Press reset");
+        }
+      }
+    
+      if (!gameOver)
+      {
+        gameStarted = true;
+        startTime = millis();
+        digitalWrite(starterLed, HIGH);
+      }
+      else
+      {
+ 
+      }
     }
     
-    if (digitalRead(button2) == 0 && gameStarted) {    
-      reactionTime = millis() - startTime;
-      gameOver = true;
-      digitalWrite(led2, HIGH);
-      Serial.print("Player 2 wins - ");
-      Serial.print(reactionTime);
-      Serial.println();
-      delay(2000);
-      digitalWrite(led2, LOW);
-      
-    }    
+    if (digitalRead(button1) == 0 && gameStarted) {
+        reactionTime =  millis() - startTime;
+        gameOver = true;
+        digitalWrite(led1, HIGH);
+        Serial.print("Player 1 wins - ");
+        Serial.print(reactionTime);
+        Serial.println();
+        delay(2000);
+        digitalWrite(led1, LOW);    
+    }
+
     
-    while (millis() < 5000)
-    {}
-    
-    gameStarted = true;
-    digitalWrite(starterLed, HIGH); 
-    
+    if (digitalRead(button2) == 0 && gameStarted) {
+        reactionTime = millis() - startTime;
+        gameOver = true;
+        digitalWrite(led2, HIGH);
+        Serial.print("Player 2 wins - ");
+        Serial.print(reactionTime);
+        Serial.println();
+        delay(2000);
+        digitalWrite(led2, LOW);
+    } 
+   
+  }
+  else
+  {
+    delay(20000);
+    digitalWrite(starterLed, LOW);
+    Serial.println("Game Over");
+    Serial.println("Press reset");
   }
 }
